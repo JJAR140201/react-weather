7645d15562ca16577b188317b34803751e24feb5
JJAR140201 <111238539+JJAR140201@users.noreply.github.com>
2023-02-10 10:58:46 -0500

    Arranque App.js

---
+++ src/app/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import WeatherInfo from "./components/weatherInfo";
+import WeatherForm from "./components/weatherForm";
+
+class App extends Component {
+    render() {
+        return (
+            <div className="container p-4">
+                <div className="row">
+                    <div className="col-md-6">
+                        <WeatherInfo/>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default App;
